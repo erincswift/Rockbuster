332a4f50a3489cef51fab8d923d22c3d0ee1ceb6
Erin Swift <130843875+erincswift@users.noreply.github.com>
2023-07-12 12:19:46 -0400

    Create TopFivePerCountryCTE.sql
    Query to determine the number of top five customers per each country

---
+++ TopFivePerCountryCTE.sql
@@ -0,0 +1,51 @@
+/*Query to determine the number of top five customers per country using CTE*/
+
+--Top five customers by total amount paid
+with top_five_cust as 
+(
+	Select payment.customer_id as customer_id,
+		country.country_id as country_id,
+		sum(payment.amount) as total_amount_pd,
+		round(avg(payment.amount),2) as avg_amount_pd
+	From payment
+	Inner Join customer on payment.customer_id = customer.customer_id
+	Inner join address on customer.address_id = address.address_id
+	Inner Join city on address.city_id = city.city_id
+	Inner Join country on city.country_id = country.country_id
+	Group by 
+		payment.customer_id,
+		country.country_id
+	Order by total_amount_pd desc
+	Limit 5
+),
+
+--Create table with total customer counts for each country
+all_customer_count (country_id, country, customer_count) as 
+(
+	Select 
+		country.country_id as country_id,
+		country.country as country,
+		count (customer_id) as customer_count
+	From customer
+	inner join address on customer.address_id = address.address_id
+	Inner Join city on address.city_id = city.city_id
+	Inner Join country on city.country_id = country.country_id
+	Group By 
+		country.country_id,
+		country.country
+	Order by customer_count DESC
+)
+  
+--Number of top 5 customers in each country
+Select 
+	all_customer_count.country,
+	all_customer_count.customer_count,
+	count (distinct top_five_cust.customer_id) as top_customer_count
+From  all_customer_count
+Left Join top_five_cust on all_customer_count.country_id = top_five_cust.country_id 
+Group by 
+	all_customer_count.country,
+	all_customer_count.customer_count
+Order by 
+	top_customer_count desc,
+	all_customer_count.customer_count desc
